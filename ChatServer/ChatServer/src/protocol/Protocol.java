51b59e4fe3b52d75d61ee01554984b00f985c13a
jmaxfield21 <jpopper21@gmail.com>
2013-11-14 22:39:41 -0700

    Update to Protocol.java and organization of informational data

--- ChatServer/ChatServer/src/protocol/Protocol.java
+++ ChatServer/ChatServer/src/protocol/Protocol.java
@@ -1,5 +1,13 @@
 package protocol;
 
-public class Protocol {
+/**
+ * Protocol.java
+ * @author Jace Maxfield && Sean Groathouse
+ * This class will be the 'Protocol Enforcer'.
+ * It will contain protocol variables as well as booleans checking to ensure
+ * the protocol is kept across the network
+ */
 
+public class Protocol {
+	
 }
