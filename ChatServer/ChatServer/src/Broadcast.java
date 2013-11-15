ec9d4be7de35fbc0f5478999d827dbe86172583e
sean223 <sean223@gmail.com>
2013-11-15 10:57:26 -0700

    Added Broadcast class.

---
+++ ChatServer/ChatServer/src/Broadcast.java
@@ -0,0 +1,26 @@
+/**
+This is the broadcast thread which pulls from the message queue in the
+server.  The server will initiate a single instance of this class
+upon its initialization, and will pass a copy of the users:sockets
+vector as well as the queue of messages.
+
+Once it pulls a message from the queue, it parses the contents
+to determine if the message is public or private and get the message
+body.  Then using the vector of users:sockets on the server, it 
+creates the appropriate responses to the recipients and sends the 
+message to their respective sockets.
+*/
+
+import java.util.concurrent.*;
+import java.util.Vector;
+
+public class Broadcast {
+	private Vector users;
+	private BlockingQueue messages;
+
+	public Broadcast(Vector initUsers, BlockingQueue initMessages) {
+		this.users = initUsers;
+		this.messages = initMessages;
+	}
+
+}
