a41442252a7965615d8c881a6e556beccf20f258
sean223 <sean223@gmail.com>
2013-11-17 22:33:06 -0700

    Working on Broadcast

--- ChatServer/ChatServer/src/server/Broadcast.java
+++ ChatServer/ChatServer/src/server/Broadcast.java
@@ -1,7 +1,7 @@
 package server;
 
 /**
-Protocol.java
+Broadcast.java
 @author Jace Maxfield && Sean Groathouse
 
 This is the broadcast thread which pulls from the message queue in the
@@ -24,16 +24,71 @@ public class Broadcast implements Runnable {
 	private Vector users;
 	private BlockingQueue messages;
 
+	private BufferedOutputStream toClient; 
+	private Socket[] sockets; // TODO: This should be tuple, but that's currently private
+
 	public Broadcast(Vector initUsers, BlockingQueue initMessages) {
 		this.users = initUsers;
 		this.messages = initMessages;
 	}
 
 	/** 
-	Reads in messages from the queue indefinitely.
+	Reads in messages from the queue indefinitely.  This will use the
+	take() method of the BlockingQueue which will block this thread 
+	indefinitely until a message is able to be removed from the queue.
 	*/
 	public void run() {
-		
+		String msg = messages.take(); // Get a message when it becomes available
+		int newLine = msg.indexOf("\r\n"); // get the index of the first occurrence of \r\n
+		String command = msg.substring(0, newLine);
+		String msgBody = msg.substring(newLine); // The \r\n and the message contents and the end-char \u0004
+		int space = command.indexOf(" "); // Find the index of the space if there is one - for "/private uname\r\n"
+		String verb = ""; // the command itself
+		String uname = ""; // the user name (if /private)
+		if (space > 0) {
+			verb = command.substring(0, space);
+			uname = command.substring(space);
+		}
+		else {
+			verb = command;
+		}
+
+		if !(verb.equalsIgnoreCase("/public") || verb.equalsIgnoreCase("/private")) {
+			// This is a bad request...
+		}
+		else if (verb.equalsIgnoreCase("/public") {
+			// send a public message
+			String response = "";
+			response += "]public ";
+			// TODO: we need to know who is sending the message...
+			response += msgBody; // this includes the \r\n, body, and \u0004
+			
+			// Specifying sockets as a parameter means the return type will be Socket[] 
+			sockets = users.toArray(sockets); 
+
+			try {
+				for (int i = 0; i < sockets.length(); i++) {
+					toClient = new BufferedOutputStream(new DataOutputStream(client.getOutputStream()));
+					// TODO: should we be storing (user, outputStream) instead of Sockets?!
+					toClient.write(response);
+					toClient.flush();
+				}
+			}
+			catch (IOException ioe) {
+				// Hopefully this won't need to be here so we won't have to decide
+				// what needs to be done...
+			}
+			finally {
+				// see above
+			}
+		}
+		else {
+			// send a private message
+			String response = "";
+			response += "]private ";
+			// TODO: add the user name who the message is from
+			response += msgBody; // this includes the \r\n, body, and \u0004
+		}
 	}
 
 }
