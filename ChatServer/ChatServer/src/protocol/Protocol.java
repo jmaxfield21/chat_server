f1bb65e298b5ce778cdb4a829187866b1d004356
jmaxfield21 <jpopper21@gmail.com>
2013-11-21 20:23:57 -0700

    Added a warning suppressor to Broadcast and deleted users from Server constructor. Finished Protocol.java

--- ChatServer/ChatServer/src/protocol/Protocol.java
+++ ChatServer/ChatServer/src/protocol/Protocol.java
@@ -11,14 +11,17 @@ package protocol;
 public class Protocol {
 
 	public static final int MAX_MESSAGE_LENGTH = 10000;
-	
+	private static final char[] ALPHA_NUMERIC = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
 	//Client side commands that are sent to the server
 	public static final String CLIENT_LOGIN = "/Login";
 	public static final String CLIENT_USER_REQUEST = "/Users";
 	public static final String CLIENT_PUBLIC_MSG = "/Public";
 	public static final String CLIENT_PRIVATE_MSG = "/Private";
 	public static final String CLIENT_CLOSE = "/Close";
-	
+
 	//Server side commands sent to all connected clients
 	public static final String SERVER_WELCOME = "]Welcome";
 	public static final String SERVER_USER_TAKEN = "]UsernameTaken";
@@ -29,30 +32,67 @@ public class Protocol {
 	public static final String SERVER_ACTIVE_USERS = "]ActiveUsers";
 	public static final String SERVER_ERROR = "]Error";
 	public static final String SERVER_BAD_SYNTAX = "]BadSyntax";
-	
+
 	//End of transmission command
 	public static final String EOT = "\u0004"; 
 	public static final char EOT_CHAR = '\u0004';
-	
+
 	public Protocol() {	}
-	
-	
+
+
 	//Validates a requested username
 	public static boolean isValidUsername(String userName) {
-		// TODO verify alphanumeric characters?
-		if(userName.length() > 16 || userName == null) {
-			return false;
+		boolean valid = false;
+		char[] userCheck = userName.toCharArray();
+		if(userName.length() < 16 && userName != null) {
+			valid = true;
 		}
-		return true;
+		for (int i = 0; i < userCheck.length; i++) {
+			boolean validChar = false;
+			for (int j = 0; j < ALPHA_NUMERIC.length; j++) {
+				if (userCheck[i] == ALPHA_NUMERIC[j]) {
+					validChar = true;
+					break;
+				}
+			}
+			if(!validChar)
+				valid = false;
+		}	
+		return valid;
 	}
-	
+
 	public static boolean isProperCommand(String command) {
-		//TODO flesh out the needed command detail
-		return true;
+		boolean correct = false;
+		char[] cmd = command.toCharArray();
+
+		for (int i = 0; i < cmd.length; i++) {
+			if(cmd[i] == '\r') {
+				String[] parsedCmd = command.split("\r\n");
+				if(parsedCmd[0].equalsIgnoreCase(CLIENT_USER_REQUEST) || parsedCmd[0].equalsIgnoreCase(CLIENT_CLOSE)) {
+					correct = true;
+					break;
+				}
+				else if(parsedCmd[0].equalsIgnoreCase(CLIENT_PUBLIC_MSG)) {
+					if(parsedCmd[parsedCmd.length].equals(EOT)) {
+						correct = true;
+						break;
+					}
+				}
+			}
+			else if(cmd[i] == ' ') {
+				String[] parsedCmd = command.split(" ");
+				if(parsedCmd[0].equalsIgnoreCase(CLIENT_LOGIN)) {
+					correct = true;
+					break;
+				}
+				else if(parsedCmd[0].equalsIgnoreCase(CLIENT_PRIVATE_MSG)) {
+					if(parsedCmd[parsedCmd.length].equals(EOT)) {
+						correct = true;
+						break;
+					}
+				}
+			}
+		}
+		return correct;
 	}
-	
-	public static String errorMsg() {
-		return "An error occured";
-	}
-	
 }
