e82cc2438ab8ad5134e4c1c294a2fc8450e561ad
sean223 <sean223@gmail.com>
2013-11-30 19:25:21 -0700

    Added most of the ChatReader class

--- ChatServer/ClientApplication/src/chatclient/ChatReader.java
+++ ChatServer/ClientApplication/src/chatclient/ChatReader.java
@@ -2,6 +2,8 @@ package chatclient;
 
 
 import java.io.*;
+import protocol.Protocol;
+import java.util.Vector;
 
 /** 
 ChatReader.java
@@ -16,21 +18,98 @@ Client methods to handle the command.
 
 public class ChatReader implements Runnable {
 	BufferedReader fromServer;
+	Client client;
 	
-	public ChatReader(Client client, BufferedReader fromTheServer) {
+	public ChatReader(Client theClient, BufferedReader fromTheServer) {
 		fromServer = fromTheServer;
+		client = theClient;
 	}
 
 	/** 
 	Listen to the server indefinitely
 	*/ 
 	public void run() {
+		String command;
+		String truncated;
+		String[] words;
+
 		while (true) {
 			try {
-				fromServer.readLine();
+				command = fromServer.readLine(); // the first line of the server's command
+				int newLine = command.indexOf("\r\n"); // get the index of the /r/n to cut it off
+				if (newLine > -1) {
+					truncated = command.substring(0, newLine);
+				}
+				else {
+					truncated = command.substring(0);
+				}
+
+				words = truncated.split(" "); // split by spaces - words[0] should be server command (starts with ])
+
+				// Handle the request based on which of the server commands this is
+				if (words[0].equalsIgnoreCase(Protocol.SERVER_WELCOME)) {
+					client.receivedWelcome();
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_USER_TAKEN)) {
+					client.receivedUsernameTaken();
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_USER_CONNECTED)) {
+					if (words.length > 1) {
+						client.receivedConnected(words[1]);
+					}
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_USER_DISCONNECTED)) {
+					if (words.length > 1) {
+						client.receivedDisconnected(words[1]);
+					}
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_ACTIVE_USERS)) {
+					if (words.length > 1) {
+						Vector<String> newUsers = new Vector<String>();
+						String[] users = words[1].split(",");
+						for (int i = 0; i < users.length; i++) {
+							newUsers.add(users[i]);
+						}
+						client.receivedActiveUsers(newUsers);
+					}
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_ERROR)) {
+					if (words.length > 1) {
+						client.receivedError(words[1]);
+					}
+					else {
+						client.receivedError("");
+					}
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_BAD_SYNTAX)) {
+					if (words.length > 1) {
+						client.receivedBadSyntax(words[1]);
+					}
+					else {
+						client.receivedBadSyntax("");
+					}
+				}
+				else if (words[0].equalsIgnoreCase(Protocol.SERVER_PUBLIC_MSG) || words[0].equalsIgnoreCase(Protocol.SERVER_PRIVATE_MSG)) {
+					if (words.length > 1) {
+						String uname = words[1];
+						String msgBody = "";
+						char serv;
+						while ((serv = (char)fromServer.read()) != Protocol.EOT_CHAR) {
+							msgBody += serv;
+						}
+						// TODO implement some kind of timeout feature?
+						if (words[0].equalsIgnoreCase(Protocol.SERVER_PUBLIC_MSG)) {
+							client.receivedPublicMsg(msgBody, uname);
+						}
+						else {
+							client.receivedPrivateMsg(msgBody, uname);
+						}
+					}
+				}
+
 			} catch (IOException e) {
 				continue;
 			}
-		}
+		} // end while
 	}
 }
