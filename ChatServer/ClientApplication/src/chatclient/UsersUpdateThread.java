65dbcec8aa54c24a77d6b73540b3409de0e46cd1
sean223 <sean223@gmail.com>
2013-12-02 21:57:03 -0700

    Added automatic refreshing for active users.

---
+++ ChatServer/ClientApplication/src/chatclient/UsersUpdateThread.java
@@ -0,0 +1,20 @@
+package chatclient;
+
+public class UsersUpdateThread implements Runnable {
+	ChatClient theGui;
+	
+	public UsersUpdateThread(ChatClient cc) {
+		theGui = cc;
+	}
+
+	public void run() {
+		while (true) {
+			theGui.softUpdateUserList();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				continue;
+			}
+		}		
+	}
+}
